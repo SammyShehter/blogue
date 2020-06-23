484404c8df14aa823aadcfa9d4a3c481984b7076
sammyshehter <samir_shehter@yahoo.com>
2020-06-23 10:44:18 +0300

    admin panel

--- admin/index.php
+++ admin/index.php
@@ -5,10 +5,34 @@ require_once '../includes/config.php';
 if(!$user->is_logged_in()){ header('Location: login.php'); }
 ?>
 
-<br>
-<br>
-<center><h1>You're inside</h1></center>
 
+<table>
+<tr>
+    <th>Title</th>
+    <th>Date</th>
+    <th>Action</th>
+</tr>
+
+<?php
+    $stmt = $db->query('SELECT postID, postCat, postDesc, postTitle, postDate FROM blogue_posts');
+    while($row = $stmt->fetch()){
+        $postDate = date('jS M Y', strtotime($row['postDate']));
+        echo "
+                <tr>
+                    <td>${row['postTitle']}</td>
+                    <td>${postDate}</td>
+                    <td>
+                    <a href='edit-post.php?id${row['postID']}'>Edit Post</a>
+                    <a href='javascript:delpost('${row['postID']}',${row['postTitle']})'>Delete Post</a>
+                    </td>
+                </tr>
+             ";
+    }
+?>
+</table>
+
+
+<!-- Logout -->
 <form action="" method="post">
 
     <button name="logout">Logout</button>
@@ -21,3 +45,4 @@ if(!$user->is_logged_in()){ header('Location: login.php'); }
         header('Location: login.php');
     }
 ?>
+<!-- Logout END-->
--- admin/login.php
+++ admin/login.php
@@ -4,12 +4,44 @@ require_once '../includes/config.php';
 //if not logged in redirecting to login page
 if($user->is_logged_in()){ header('Location: index.php'); }
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../style/style.css">
+    <title>Blogue_Login</title>
+</head>
+<body>
+    <div class='authForm flex-center'>
+        <form action="" method="post">
+            <input type="text" name="username" placeholder="Username" value="" />
+            <input type="password" name="password" placeholder="Password" value="" />
+            <button type="submit" name="submit">Login</button>
+        </form>
+    </div>
+</body>
+<script>
 
-<form action="" method="post">
-<p><label>Username</label><input type="text" name="username" value=""  /></p>
-<p><label>Password</label><input type="password" name="password" value=""  /></p>
-<p><label></label><input type="submit" name="submit" value="Login"  /></p>
-</form>
+const body = document.querySelector('body');
+
+async function date() {
+    let clock = new Date().getHours();
+
+    if(clock > 7 && clock < 19){
+        body.style.background = 'url("./admin_scss/background/12.jpg") center';
+        body.style.backgroundSize = 'cover';
+    }else{
+        body.style.background = 'url("./admin_scss/background/24.jpg") center';
+        body.style.backgroundSize = 'cover';
+    }
+} 
+date();
+setInterval(date,1000*60*60);
+</script>
+</html>
+
+
 <?php
 
 //process login form if submitted
