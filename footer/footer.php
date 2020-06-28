f3fba188bb0b1499ac73ae2adc9a1b3d246e50fe
sammyshehter <samir_shehter@yahoo.com>
2020-06-28 12:56:19 +0300

    done

--- footer/footer.php
+++ footer/footer.php
@@ -1,9 +1,7 @@
     <div class="socials flex-center">
-    <a href="#"><i class="fa fa-facebook"></i></a>
-    <a href="#"><i class="fa fa-facebook"></i></a>
-    <a href="#"><i class="fa fa-facebook"></i></a>
-    <a href="#"><i class="fa fa-facebook"></i></a>
-    <a href="#"><i class="fa fa-facebook"></i></a>
+    <a href="facebook.com/sammyshehter/"><i class="fa fa-facebook"></i></a>
+    <a href="github.com/SammyShehter"><i class="fa fa-github"></i></a>
+    <a href="vk.com/sammyshehter"><i class="fa fa-vk"></i></a>
     </div>
 
 </body>
