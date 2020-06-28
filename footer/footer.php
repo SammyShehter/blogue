683b607c12ba809b65e4c3bf75ba4fbd9ad5fa16
sammyshehter <samir_shehter@yahoo.com>
2020-06-28 13:24:03 +0300

    done

--- footer/footer.php
+++ footer/footer.php
@@ -2,7 +2,6 @@
     <a target='_blank' href="https://www.facebook.com/sammyshehter/"><i class="fa fa-facebook-official"></i></a>
     <a target='_blank' href="https://github.com/SammyShehter"><i class="fa fa-github"></i></a>
     <a target='_blank' href="https://www.vk.com/sammyshehter"><i class="fa fa-vk"></i></a>
-    <a target='_blank' href="fb://https://www.facebook.com/sammyshehter/"><i class="fa fa-facebook"></i></a>
     </div>
 
 </body>
