56e9ff19d85481c72bb33f97adf252294baee5d5
Sammy Shehter <samir_shehter@yahoo.com>
2021-01-22 10:58:48 +0200

    testtest2

--- header/header.js
+++ header/header.js
@@ -1,27 +1,27 @@
  'use strict'
 
- const mobileMenuCall = document.querySelector('.mobile-menu-call');
- const menu = document.querySelector('.menu');
- const submenuCall = document.querySelector('.menu-active');
- const submenu = document.querySelector('.submenu');
+ const mobileMenuCall = document.querySelector('.mobile-menu-call')
+ const menu = document.querySelector('.menu')
+ const submenuCall = document.querySelector('.menu-active')
+ const submenu = document.querySelector('.submenu')
 
  window.addEventListener("resize", () => {
      if (document.body.clientWidth < 1024) {
-         menu.classList.add('hide');
+         menu.classList.add('hide')
      } else {
-         menu.classList.remove('hide');
-         menu.classList.remove('show');
-         submenu.classList.remove('show');
+         menu.classList.remove('hide')
+         menu.classList.remove('show')
+         submenu.classList.remove('show')
      }
- });
+ })
 
  mobileMenuCall.addEventListener('click', () => {
-     menu.classList.toggle('show');
- });
+     menu.classList.toggle('show')
+ })
 
 
  submenuCall.addEventListener('click', () => {
      if (document.body.clientWidth <= 1024) {
-         submenu.classList.toggle('show');
+         submenu.classList.toggle('show')
      }
- });
+ })
